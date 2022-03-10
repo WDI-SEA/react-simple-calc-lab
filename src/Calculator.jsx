fe38490e180f99f80a02c6de92789160105ca110
“jamelscott” <itsjamelscott@gmail.com>
2022-03-09 21:17:18 -0800

    MVP + BONUS

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -5,7 +5,7 @@ export default class Calculator extends Component {
     state={
         num1:"1",
         num2:"1",
-        sum: ""
+        sum: "see results here"
 
     }
 
@@ -14,11 +14,24 @@ export default class Calculator extends Component {
         this.setState({ [num]: e.target.value})
     }
 
-    setSum = (num1, num2)=>{
-        console.log(this.state.sum)
-        this.setState ({sum: parseInt(num1)+parseInt(num2)})
+    // setSum = (num1, num2)=>{
+    //     console.log(this.state.sum)
+    //     this.setState ({sum: parseInt(num1)+parseInt(num2)})
+    // }
+    addNum = (num1,num2)=>{
+        this.setState({sum: parseInt(num1)+parseInt(num2)})
     }
-
+    subNum = (num1,num2)=>{
+        this.setState({sum: parseInt(num1)-parseInt(num2)})
+    }
+    mulitplyNum = (num1,num2)=>{
+        this.setState({sum: parseInt(num1)*parseInt(num2)})
+    }
+    divideNum = (num1,num2)=>{
+        this.setState({sum: parseInt(num1)/parseInt(num2)})
+    }
+    
+    
 
   render() {
     return (
@@ -28,9 +41,14 @@ export default class Calculator extends Component {
         <div className="add">
         
           <input name = "num1"type="number" value={this.state.num1} onChange={ (e)=> this.setNum(e,"num1")}/>
-          <span>+</span>
+          <div>
+              <button onClick={(e)=>{this.addNum(this.state.num1, this.state.num2)}}>Add</button>
+              <button onClick={(e)=>{this.subNum(this.state.num1, this.state.num2)}} >Subtract</button>
+              <button onClick={(e)=>{this.mulitplyNum(this.state.num1, this.state.num2)}}>Multiply</button>
+              <button onClick={(e)=>{this.divideNum(this.state.num1, this.state.num2)}}>Divide</button>
+          </div>
           <input name="num2" type="number" value={this.state.num2} onChange={ (e)=> this.setNum(e,"num2")}/>
-          <button name='buttonSum' onClick={ (e)=>{this.setSum(this.state.num1, this.state.num2) }} >=</button>
+          
           <h3>{this.state.sum}</h3>
         </div>
       </div>
