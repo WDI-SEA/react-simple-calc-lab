bc7f249990f541e7ab054eecac8096aba9c4df66
justin-aquino <justin.aquino25@gmail.com>
2022-03-09 22:55:04 -0800

    refactoring code

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,13 +9,13 @@ class Calculator extends Component {
     error: ""
   }
 
-calculate = (e) => {
-  e.preventDefault()
-  this.setState({
-    num1: "",
-    num2: "",
-  })
-}
+// calculate = (e) => {
+//   e.preventDefault()
+//   this.setState({
+//     num1: "",
+//     num2: "",
+//   })
+// }
 
 textChangeHandler = (e) => {
   const updatedInput = {
@@ -29,50 +29,87 @@ textChangeHandler = (e) => {
   }, ()=> console.log(this.state))
 }
 
-addNums = () => {
-  if(!this.state.num1 || !this.state.num2) {
+calculate = (e) => {
+  e.preventDefault()
+  if(!this.state.num1 || !this.state.num2){
     this.setState({
-      error: "Please fill in both number fields."
+      error: "Please fill in both number fields"
     })
   } else {
-    this.setState({
-      result: this.state.num1 + this.state.num2
-    })
+    if( e.target.name === "+"){
+      this.setState({
+        result: this.state.num1 + this.state.num2,
+        num1: "",
+        num2: "",
+        
+      })
+    } else if (e.target.name === "-"){
+      this.setState({
+        result: this.state.num1 - this.state.num2,
+        num1: "",
+        num2: "",
+      })
+    } else if (e.target.name === "*") {
+      this.setState({
+        result: this.state.num1 * this.state.num2,
+        num1: "",
+        num2: "",
+      })
+    } else {
+      this.setState({
+        result: this.state.num1 / this.state.num2,
+        num1: "",
+        num2: "",
+      })
+    }
   }
 }
-subtractNums = () => {
-  if(!this.state.num1 || !this.state.num2) {
-    this.setState({
-      error: "Please fill in both number fields."
-    })
-  } else {
-  this.setState({
-    result: this.state.num1 - this.state.num2
-  })
-  }
-}
-multiplyNums = () => {
-  if(!this.state.num1 || !this.state.num2) {
-    this.setState({
-      error: "Please fill in both number fields."
-    })
-  } else {
-    this.setState({
-    result: this.state.num1 * this.state.num2
-    })
-  }
-}
-divideNums = () => {
-  if(!this.state.num1 || !this.state.num2) {
-    this.setState({
-      error: "Please fill in both number fields."
-    })
-  } else {
-    this.setState({
-      result: this.state.num1 / this.state.num2
-    })
-  }
-}
+
+// addNums = (e) => {
+//   console.log(e.target.name)
+//   if(!this.state.num1 || !this.state.num2) {
+//     this.setState({
+//       error: "Please fill in both number fields."
+//     })
+//   } else {
+//     this.setState({
+//       result: this.state.num1 + this.state.num2
+//     })
+//   }
+// }
+// subtractNums = () => {
+//   if(!this.state.num1 || !this.state.num2) {
+//     this.setState({
+//       error: "Please fill in both number fields."
+//     })
+//   } else {
+//   this.setState({
+//     result: this.state.num1 - this.state.num2
+//   })
+//   }
+// }
+// multiplyNums = () => {
+//   if(!this.state.num1 || !this.state.num2) {
+//     this.setState({
+//       error: "Please fill in both number fields."
+//     })
+//   } else {
+//     this.setState({
+//     result: this.state.num1 * this.state.num2
+//     })
+//   }
+// }
+// divideNums = () => {
+//   if(!this.state.num1 || !this.state.num2) {
+//     this.setState({
+//       error: "Please fill in both number fields."
+//     })
+//   } else {
+//     this.setState({
+//       result: this.state.num1 / this.state.num2
+//     })
+//   }
+// }
   render(){
     return (
       <div className="App App-header">
@@ -96,10 +133,10 @@ divideNums = () => {
             onChange={this.textChangeHandler}
           /> <br />
           <p className="errorMessage">{this.state.error}</p>
-          <button type="submit" onClick={this.addNums}>+</button>
-          <button type="submit" onClick={this.subtractNums}>-</button>
-          <button type="submit" onClick={this.multiplyNums}>x</button>
-          <button type="submit" onClick={this.divideNums}>/</button>
+          <button type="submit" name="+" onClick={this.calculate}>+</button>
+          <button type="submit" name="-" onClick={this.calculate}>-</button>
+          <button type="submit" name="*" onClick={this.calculate}>x</button>
+          <button type="submit" name="/" onClick={this.calculate}>/</button>
           <h2> = {this.state.result}</h2>
           
         </form>
