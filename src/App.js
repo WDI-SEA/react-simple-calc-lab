7b8c0a983c1d3618d38198c7368ed4a79f440d61
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-03-10 10:45:24 -0800

    finish deliverable

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Calculator from "./Calculator.js";
+
+function App() {
+    return (
+        <Calculator />
+    )
+}
+
+export default App
+
--- src/Calculator.js
+++ src/Calculator.js
@@ -3,9 +3,52 @@ import React, { Component } from 'react';
 export default class Calculator extends Component {
    
     state = {
-        num1: 0,
-        num2: 0,
-        result: 0
+        num1: null,
+        num2: null,
+        result: 0,
+        operator: "+"
+    }
+    calculate = (e) => {
+        e.preventDefault()
+        if (this.state.num1 === null || this.state.num2 === null) return console.log("nope")
+        // this.setState({
+        //     result: this.state.num1 + this.state.num2
+        // })
+        
+        this.setState(prevState => {
+            let operation = 0
+            switch (this.state.operator) {
+                case '+':
+                    operation = prevState.num1 + prevState.num2
+                    break
+                case '-':
+                    operation = prevState.num1 - prevState.num2
+                    break
+                case '*':
+                    operation = prevState.num1 * prevState.num2
+                    break
+                case '/':
+                    operation = prevState.num1 / prevState.num2
+                    break
+                default: 
+                    console.log("I have never heard of that math before!")
+                    break
+
+            }
+            return {
+                result: operation
+            }
+        })
+    }
+
+    setNum = (e) => {
+        this.setState({[e.target.name]: parseInt(e.target.value)})
+    }
+
+    handleSetOp = (e) => {
+        this.setState({
+            operator: e.target.name
+        })
     }
 
     render() {
@@ -13,18 +56,52 @@ export default class Calculator extends Component {
             <div className="App">
                 <h1>Add With React</h1>
 
-                <div>
+                <form onSubmit={this.calculate}>
                     <input type="number"
-                    name="num1"
-                    placeholder="Enter your first number"
-                    value={this.state.num1}
-                    onChange={(e) =. this.setNum(e, 'num1') }
+                        name="num1"
+                        placeholder="Enter your first number"
+                        value={this.state.num1 || ''}
+                        onChange={this.setNum}
                     />
                     <span>+</span>
-                    <input type='text' />
+                    <input type="number"
+                        name="num2"
+                        placeholder="Enter your second number"
+                        value={this.state.num2 || ''}
+                        onChange={this.setNum} 
+                    />
                     <button>=</button>
-                    <h3>Addition Results Go Here!</h3>
-                </div>
+                    <h3>{this.state.result}</h3>
+                </form>
+
+                <button 
+                    name="+"
+                    onClick={this.handleSetOp}
+                    >
+                        +
+                </button>
+
+                <button 
+                    name="-"
+                    onClick={this.handleSetOp}
+                    >
+                        -
+                </button>
+
+                <button 
+                    name="*"
+                    onClick={this.handleSetOp}
+                    >
+                        *
+                </button>
+
+                <button 
+                    name="/"
+                    onClick={this.handleSetOp}
+                    >
+                        /
+                </button>
+
             </div>
         )
     }
