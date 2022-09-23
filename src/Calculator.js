f1a8e9d01a8ad281f78796f4a250ffdbbf5af16b
JasonXChen <jasonxchen888@gmail.com>
2022-09-23 19:19:01 -0400

    Implement subtraction, multiplication, and division operations

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,36 +3,77 @@ import React, { Component } from "react";
 class Calculator extends Component {
     state = {
       num1: "",
+      operator: "",
       num2: "",
       sum: ""
     }
-    setNum = e => {
+    setValue = e => {
         this.setState({[e.target.name]: e.target.value});
     }
-    addNums = e => {
+    performOperation = e => {
         e.preventDefault();
         this.setState(prevState => {
-            return {
-                sum: parseInt(prevState.num1) + parseInt(prevState.num2),
-                num1: "",
-                num2: ""
+            switch (this.state.operator) {
+                case "+":
+                    return {
+                        sum: parseInt(prevState.num1) + parseInt(prevState.num2),
+                        num1: "",
+                        operator: "",
+                        num2: ""
+                    }
+                    break;
+                case "-":
+                    return {
+                        sum: parseInt(prevState.num1) - parseInt(prevState.num2),
+                        num1: "",
+                        operator: "",
+                        num2: ""
+                    }
+                    break;
+                case "*":
+                    return {
+                        sum: parseInt(prevState.num1) * parseInt(prevState.num2),
+                        num1: "",
+                        operator: "",
+                        num2: ""
+                    }
+                    break;
+                case "/":
+                    return {
+                        sum: parseInt(prevState.num1) / parseInt(prevState.num2),
+                        num1: "",
+                        operator: "",
+                        num2: ""
+                    }
+                    break;
+                default:
+                    return {
+                        sum: "Invalid arithmetic operator",
+                        num1: "",
+                        operator: "",
+                        num2: ""
+                    }
+                    break;
             }
         });
     }
     render() {
         return (
             <div className="container">
-                <h1>Add with React!</h1>
-                <div className="add">
-                    <form onSubmit={this.addNums}>
+                <h1>Do maths with React!</h1>
+                <div>
+                    <form onSubmit={this.performOperation}>
                         <input type="number" name="num1" placeholder="Enter your first number" 
                             value={this.state.num1} 
-                            onChange={this.setNum}
+                            onChange={this.setValue}
                         />
-                        <span>+</span>
+                        <input type="text" name="operator" placeholder="+, -, *, /" 
+                            value={this.state.operator} 
+                            onChange={this.setValue}
+                        />
                         <input type="number" name="num2" placeholder="Enter your second number" 
                             value={this.state.num2} 
-                            onChange={this.setNum}
+                            onChange={this.setValue}
                         />
                         <button type="submit">=</button>
                     </form>
