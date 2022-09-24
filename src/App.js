149c0d3f08f9759f706388a707c64b23564b013d
prijacash <prijacash@gmail.com>
2022-09-23 21:30:49 -0700

    updated step2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import Calculator from './Calculator';
 
 class App extends Component {
 
+  state = {
+    taskArray: this.props.Calculator
+  }
+
+  // change handler event
+  onChange = (e) => {
+    console.log('input was changed')
+  }
+
+  //capture values on onChange property
+  addValue = (e) => {
+    e.preventDefault()
+      console.log('numbers inputed')
+  }
+
   render() {
+
+
     return (
       <div className="App">
         <Calculator
