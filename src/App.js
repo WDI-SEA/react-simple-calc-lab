6ab0b4e124e68e0ed02b473cb8c94130cae446c8
elek8871 <elekmichelle@gmail.com>
2022-09-24 12:25:17 -0600

    completes MVP for deliverable, state and function were moved to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import Calculator from "./Calculator"
 
+export default class App extends Component {
+    // state will need empty arrays for input ?and answer
+   //  state/functions go in app.js
+   state={
+    numOne: " ",
+    numTwo: " ",
+    sum: " "
+  }
+  // event listener
+  setNum = (e, num) =>{
+    console.log(e.target)
+    // brackets allow a dynamic key value which becomes numOne or numTwo depending on what was clicked
+    this.setState({ [e.target.name]:e.target.value})
+    console.log(this.state.numOne)
+  }
+  // math function
+  addition = (numOne, numTwo) =>{
+    return numOne + numTwo
+  }
+  
+  getSum = e =>{
+    e.preventDefault()
+    console.log (this.state.numOne, this.state.numTwo)
+    //get sum, parseInt changes from a string to an integer
+    this.setState({
+      sum: this.addition(parseInt(this.state.numOne), parseInt(this.state.numTwo))
+    }
+    )
+    console.log(this.state)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    
+    return(
+      <div>
+        <Calculator 
+        numOne={this.state.numOne}
+        numTwo={this.state.numTwo}
+        setNum={this.setNum}
+        getSum={this.getSum}
+        sum ={this.state.sum}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+
+
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,45 +1,36 @@
 import React, { Component} from "react"
 
 
-// class -> state -> event trackers->fnxs
-// render -> new data
-// return -> html
-
-
 export default class Calculator extends Component{
-    // state will need empty arrays for input ?and answer
-    state={
-        numOne:" ",
-        numTwo: " ",
-        answer: " "
-    }
-    // need event trackers
-    setNum = e =>{
-        e.preventDefault()
-        // brackets allow a dynamic key value which becomes numOne or numTwo depending on what was clicked
-        this.setState({ [e.target.name]:e.target.value})
-    }
+    render() {
 
-    render(){
-    // this.state.numOne + this.state.numTwo
+        return(
+            < div className="container">
+                <h1> Add with React!</h1>
 
-    return(
-        < div className="container">
-        <div className="add">
-            <h1> Add with React!</h1>
-                <input type="text"
-                 name="numOne"
-                 value={this.state.numOne}
-                 onChange={this.setNum} />
-                <span>+</span>
-                <input type="text"
-                name="numTwo"
-                value={this.state.numTwo}
-                onChange={this.setNum} />
-                <button>=</button>
-            <h3> Addition results go here: </h3>
+            <div className="add">
+                <form onSubmit={ (e) => this.props.getSum(e)}>
+                    <input 
+                        type="number"
+                        name="numOne"
+                        onChange= { (e)=> this.props.setNum(e, "num1")}
+                        value={this.props.numOne}
+                    />
+
+                    <span> + </span>
+
+                    <input
+                        type="number"
+                        name="numTwo"
+                        onChange= { (e)=> this.props.setNum(e, "num2")}
+                        value={this.props.numTwo}
+                    />
+
+                    <button> = </button>
+                </form>
+                <h3> Answer: {this.props.sum} </h3>
+            </div>
         </div>
-    </div>
     )
-    }
+}
 }
