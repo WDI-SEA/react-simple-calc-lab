beeb77888efe79ac38e706ffdeb5cb91cb38c267
devin-lynch <devin.l.lynch@gmail.com>
2022-09-23 19:49:17 -0500

    Create calc.jsx and set appjs to class

---
+++ src/Calculator.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+
+export default class Calculator extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        <div className="container">
+            <h1>Add with React!</h1>
+
+            <div className="add">
+                <input
+                    type="text"
+                />
+
+                <span>+</span>
+
+                <input
+                    type="text"
+                />
+
+                <button>=</button>
+                
+                <h3>Addition results go here!</h3>
+            </div>
+        </div>
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
