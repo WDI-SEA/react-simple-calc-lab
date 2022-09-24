d46dde2175efc10a6bbab8cccde9b08134df4cb9
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-09-23 22:33:20 -0700

    finished calc

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,10 +4,42 @@ import React, { Component } from 'react'
 class Calculator extends Component {
 
     state = {
-        numInp: '',
+        numInp1: '',
+        numImp2: '',
         operand: '',
-        result: ''
+        result: '',
     }
+
+
+    setOp = op => {
+        this.setState({
+            operand: op
+        })
+    }
+
+    calculate = () => {
+        let num1 = this.state.numInp1
+        let num2 = this.state.numInp2
+        let operand = this.state.operand
+        let result = ''
+        if(operand === '+') {
+            result = Number(num1) + Number(num2)
+        } else if (operand === '-') {
+            result = num1 - num2
+        } else if (operand === '*') {
+            result = num1 * num2
+        } else if (operand === '/') {
+            result = num1 / num2
+        }
+
+        this.setState({
+            result: result
+        })
+    }
+
+    setNum = (e, num) => {
+        this.setState({ [num]: e.target.value});
+      }
 
     render() {
         return (
@@ -15,11 +47,27 @@ class Calculator extends Component {
                 <h1>Add with React!</h1>
 
                 <div className='add'>
-                    <input type='text' />
-                    <span>+</span>
-                    <input type="text" />
-                    <button>=</button>
-                    <h3>Addition results go here!</h3>
+                    <input type='number' 
+                    name='numInp1'
+                    placeholder='First Number'
+                    value={this.state.numInp1}
+                    onChange={ (e) => this.setNum(e, 'numInp1')}
+                    />
+                    <span>{this.state.operand}</span>
+                    <input type="number" 
+                     name='numInp2'
+                     placeholder='Second Number'
+                     value={this.state.numInp2}
+                     onChange={ (e) => this.setNum(e, 'numInp2')}
+                    />
+                    <button onClick={this.calculate}>=</button>
+                    <div>
+                    <button value='+' onClick={e => this.setOp(e.target.value)}>+</button>
+                    <button value='-' onClick={e => this.setOp(e.target.value)}>-</button>
+                    <button value='*' onClick={e => this.setOp(e.target.value)}>*</button>
+                    <button value='/' onClick={e => this.setOp(e.target.value)}>/</button>
+                    </div>
+                    <h3>The Answer! {this.state.result}</h3>
                 </div>
             </div>
         )
