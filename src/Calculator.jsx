acaa0e3e46a856e65c9ac4079908eb88c5e4b6f9
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-09-23 21:25:09 -0700

    setup complete.

---
+++ src/Calculator.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+
+class Calculator extends Component {
+
+    state = {
+        numInp: '',
+        operand: '',
+        result: ''
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <h1>Add with React!</h1>
+
+                <div className='add'>
+                    <input type='text' />
+                    <span>+</span>
+                    <input type="text" />
+                    <button>=</button>
+                    <h3>Addition results go here!</h3>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Calculator
