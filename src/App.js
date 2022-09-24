0ec7eb8c88547fd1338600ed9ff7653ae7e72c85
Moodybleu <meganbenn27@icloud.com>
2022-09-24 09:02:33 -0700

    Inital commit, classes built

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render(){
+    return(
+      <>
+      
+      <h1>Hi I'm the H1 in the App file</h1>
+      
+      </>
+    )
+  }
+}
---
+++ src/Calculator.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+export default class Calculator extends Component {
+    render(){
+
+        <div className="container">
+        <h1>Add with React!</h1>
+      
+        <div className="add">
+          <input type="text" />
+          <span>+</span>
+          <input type="text" />
+          <button>=</button>
+          <h3>Addition results go here!</h3>
+        </div>
+      </div>   
+
+        return(
+            <>
+            
+            </>
+        )
+    }
+}
