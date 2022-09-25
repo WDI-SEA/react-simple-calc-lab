2dd94989eecab6a25411b037509a93da8ea7703c
prijacash <prijacash@gmail.com>
2022-09-25 12:44:53 -0700

    deliverable done, updated with full calc

--- src/App.js
+++ src/App.js
@@ -3,42 +3,12 @@ import './App.css';
 import Calculator from './Calculator';
 
 class App extends Component {
-
-  state = {
-    taskArray: this.props.Calculator
-  }
-
-  // change handler event
-  onChange = (e) => {
-    console.log('input was changed')
-    this.setState({
-      newItem: e.target.value
-    })
-  }
-
-  //capture values on onChange property
-  addValue = (e) => {
-    e.preventDefault()
-      console.log('numbers inputed')
-    this.setState(prevState => {
-      
-    })
-
-  }
-
   render() {
-
-
     return (
-      <div className="App">
-        <Calculator
-        
-        />
-      </div>
-    );
-    
+      <Calculator />
+    )
   }
-
 }
+
 
 export default App;
--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,18 +2,76 @@ import React, { Component } from 'react'
 import './App.css'
 
 class Calculator extends Component {
+    state = {
+        num1: '',
+        num2: '',
+        operation: "+",
+        sum: ''
+    }
+
+    setNum = (e) => {
+        this.setState({
+            [e.target.name]: Number.parseInt(e.target.value)
+        })
+    }
+
+    setOperation = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    setResult = (e) => {
+        let calculation = 0
+        if (this.state.operation === "+") {
+            calculation = this.state.num1 + this.state.num2
+        } else if (this.state.operation === "-") {
+            calculation = this.state.num1 - this.state.num2
+        } else if (this.state.operation === "*") {
+            calculation = this.state.num1 * this.state.num2
+        } else {
+            calculation = this.state.num1 / this.state.num2
+        }
+        this.setState({
+            result: calculation
+        })
+    }
+
     render() {
         return (
             <div className="container">
-            <h1>Add with React!</h1>
+            <h1>Calculate with React!</h1>
 
-            <div className="add">
-                <input type="text" />
-                <span>+</span>
-                <input type="text" />
-                <button>=</button>
-                <h3>Addition results go here!</h3>
-            </div>
+                <div className="add">
+                    <input 
+                        type="number" 
+                        name="num1"
+                        placeholder="Enter your first number"
+                        value={this.state.num1}
+                        onChange={this.setNum}
+                    />
+                    
+                    <select name="operation" onChange={this.setOperation}>
+                        <option value="+">+</option>
+                        <option value="-">-</option>
+                        <option value="*">*</option>
+                        <option value="/">/</option>
+                    </select>
+
+                    <input 
+                        type="number" 
+                        name="num2"
+                        placeholder="Enter your 2nd number"
+                        value={this.state.num2}
+                        onChange={this.setNum}
+                    />
+
+                    <button onClick={this.setResult}>=</button>
+                    
+                    <h3>Calculations results go here!</h3>
+
+                    <p>{this.state.result}</p>
+                </div>
             </div>
         )
     }
