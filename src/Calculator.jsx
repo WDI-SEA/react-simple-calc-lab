f556c1c38b060c1c65987de6b20daf814645e948
syorn96 <syorn2014@gmail.com>
2022-09-25 12:46:07 -0700

    calculator works with select/options

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,21 +3,28 @@ import React, { Component } from 'react'
 export default class Calculator extends Component {
     
     state = {
-        num1: '',
-        num2: '',
+        num1: "",
+        num2: "",
+        operator: "",
         results: ""
     }
     setNum = (e, num) => {
         this.setState({ [num]:e.target.value})
         console.log(e)
     }
-    addNum = e => {
-        this.setState((prevState => {
-            console(prevState.num1)
-            return {
-                results: prevState.results
+    addNum = () => {
+        this.setState((prevState, props) => {
+            // console(prevState.num1)
+            if(prevState.operator == "+"){
+            return { results: parseInt(prevState.num1) + parseInt(prevState.num2) } 
+            }else if(prevState.operator == "-"){
+                return { results: parseInt(prevState.num1) - parseInt(prevState.num2) } 
+            } else if (prevState.operator == "x") {
+                return { results: parseInt(prevState.num1) * parseInt(prevState.num2) } 
+            } else if (prevState.operator == "/") {
+                return { results: parseInt(prevState.num1) / parseInt(prevState.num2) }
             }
-        }))
+        })
     }
 
     render() {
@@ -30,14 +37,19 @@ export default class Calculator extends Component {
                     value={this.state.num1}
                     onChange={ (e)=> this.setNum(e, 'num1')}
                     placeholder="Enter your 1st number"/>
-                    <span>+</span>
+                    <select type="text" name="" value={this.state.operator} onChange={ (e)=> this.setNum(e, 'operator')}>
+                        <option value="+">+</option>
+                        <option value="-">-</option>
+                        <option value="x">x</option>
+                        <option value="/">/</option>
+                    </select>
                     <input type="number"
                     name="num1"
                     value={this.state.num2}
                     onChange={ (e)=> this.setNum(e, 'num2')}
                     placeholder="Enter your 2nd number"/>
                     <button onClick={this.addNum} type="submit">=</button>
-                    <h3>{this.state.results}</h3>
+                    <h3>Results: {this.state.results}</h3>
                 </div>
             </div>
         )
