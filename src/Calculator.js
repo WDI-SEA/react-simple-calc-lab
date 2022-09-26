c2972f5de49425bada0f99e91f82048769bfeaa3
aimeepham14 <aimee.pham14@gmail.com>
2022-09-26 09:38:22 -0700

    revised deliverable

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,22 +3,28 @@ import React, { Component } from 'react'
 class Calculator extends Component {
 
 state= {
-    num1: '',
-    num2: '',
-    sum: ''
+    num1: 0,
+    num2: 0,
+    sum: 0
 };
 
 
 //function to handle the changes of the state
-setNum = (e) => {
-    // console.log(e.target)
-    this.setState({ [e.target.value]: (e.target.value)});
+setNum = (e,num) => {
+    // console.log(num, e)
+    this.setState({ [num]: Number(e.target.value)});
   };
 
-addNumber = () => {
-    let num1 = Number(this.state.num1);
-    let num2 = Number(this.state.num2);
-    this.setState({sum: num1 + num2 });
+calculate = () => {
+    //take the two values from state
+    //add them together
+    this.setState(prevState => {
+        const addition = prevState.num1 + prevState.num2
+        return {
+            //set the result in state to be the new value
+            sum: addition
+        }
+    })
 };
 
 
@@ -30,13 +36,13 @@ render() {
             <h1>Add with React!</h1>
 
             <div className="add">
-                <input type="number" name="num1" placeholder="Enter a number" value={this.state.num1}
-                    onChange={this.setNum} />
+                <input type="text" name="num1" placeholder="Enter a number" value={this.state.num1}
+                    onChange={e => this.setNum(e, 'num1')} />
                 <span>+</span>
-                <input type="number" name="num2" placeholder="Enter a number" value={this.state.num2}
-                    onChange={this.setNum} />
-                <button onClick={this.addNumber}>=</button>
-                <h3>= {this.state.addNumber}</h3>
+                <input type="text" name="num2" placeholder="Enter a number" value={this.state.num2}
+                    onChange={ e => this.setNum(e, 'num2')} />
+                <button onClick={this.calculate}>=</button>
+                <h3>= {this.state.sum}</h3>
             </div>
         </div>
     )
