9c3dd727d9ac745ff77dd7cc8bd7d3b52d2a5f17
aimeepham14 <aimee.pham14@gmail.com>
2022-09-25 19:47:36 -0700

    finished deliverable

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
 
-// state= {
-//     num1: i,
-//     num2: i
-
-// }
-
-
+state= {
+    num1: '',
+    num2: '',
+    sum: ''
+};
 
 
+//function to handle the changes of the state
+setNum = (e) => {
+    // console.log(e.target)
+    this.setState({ [e.target.value]: (e.target.value)});
+  };
+
+addNumber = () => {
+    let num1 = Number(this.state.num1);
+    let num2 = Number(this.state.num2);
+    this.setState({sum: num1 + num2 });
+};
 
 
 
@@ -21,11 +30,13 @@ render() {
             <h1>Add with React!</h1>
 
             <div className="add">
-                <input type="text" />
+                <input type="number" name="num1" placeholder="Enter a number" value={this.state.num1}
+                    onChange={this.setNum} />
                 <span>+</span>
-                <input type="text" />
-                <button>=</button>
-                <h3>Addition results go here!</h3>
+                <input type="number" name="num2" placeholder="Enter a number" value={this.state.num2}
+                    onChange={this.setNum} />
+                <button onClick={this.addNumber}>=</button>
+                <h3>= {this.state.addNumber}</h3>
             </div>
         </div>
     )
