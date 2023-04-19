e49e688fbae3895fcbbd19b9993d29c5c4fee101
jaronw1 <jaronwong6@gmail.com>
2023-04-19 09:26:42 -0700

    deli

---
+++ src/Calculator.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+
+export default class Calculator extends Component {
+
+    state = {
+        num1: 0,
+        num2: 0
+   
+    }
+
+    setNum =(e, num) => {
+        this.setState({[num]: e.target.value})
+    }
+
+    handleSum = () => {
+        let num1 = parseInt(this.state.num1)
+        let num2 = parseInt(this.state.num2)
+        this.setState(()=> {
+            return {
+                sum: num1 + num2
+            }
+        })
+    }
+
+
+
+
+
+
+    render () {
+        return(
+            
+            <div className="container">
+                <h1>Add with React!</h1>
+
+                <input type="number"
+                    name="num1"
+                    placeholder="Enter your first number"
+                    value={this.state.num1}
+                    onChange={ (e) => this.setNum(e, 'num1') }
+                />
+                <span>+</span>
+                    <input type="number"
+                    name="num2"
+                    placeholder="Enter your first number"
+                    value={this.state.num1}
+                    onChange={ (e) => this.setNum(e, 'num1') }
+                    />
+
+                     <button onClick={this.handleSum}>=</button>
+                    <h3>{this.state.sum}</h3>
+            </div>
+        )
+    }
+}
