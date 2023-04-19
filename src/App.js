54e384ff19f4bbfcb1b29cac323e109e0211f565
mylesw27 <myleswiegel27@gmail.com>
2023-04-19 09:45:21 -0700

    finish deliverable

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, {Component, component} from 'react'
+import Calculator from './Calculator'
+
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    )
+  }
+}
+
+export default App;
