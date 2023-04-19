24b4eab8ed490de138156b666da398def1428030
juanedcabrera <juanedcabrera@gmail.com>
2023-04-18 18:11:18 -0600

    Step 3

--- simple-calc/src/Calculator.js
+++ simple-calc/src/Calculator.js
@@ -9,6 +9,12 @@ class Calculator extends Component {
       num2: '',
       result: ''
     };
+
+    this.setNum = this.setNum.bind(this);
+  }
+
+  setNum(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
@@ -16,9 +22,9 @@ class Calculator extends Component {
       <div className="container">
         <h1>Add with React!</h1>
         <div className="add">
-          <input type="text" value={this.state.num1} />
+          <input type="text" name="num1" value={this.state.num1} onChange={this.setNum} />
           <span>+</span>
-          <input type="text" value={this.state.num2} />
+          <input type="text" name="num2" value={this.state.num2} onChange={this.setNum} />
           <button>=</button>
           <h3>{this.state.result}</h3>
         </div>
