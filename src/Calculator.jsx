e021cbd8885ef80b18547212667e2313761ca6fa
Heiner000 <garrett.heiner@gmail.com>
2023-04-19 10:07:53 -0700

    adds sub,mult,div

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -14,9 +14,27 @@ export default class Calculator extends Component {
         })
     }
 
-    handleSubmit = () => {
+    handleSum = () => {
         const { num1, num2 } = this.state
-        const result = parseFloat(num1) + parseFloat(num2)
+        const result = parseInt(num1) + parseInt(num2)
+        this.setState({ result })
+    }
+
+    handleSubtract = () => {
+        const { num1, num2 } = this.state
+        const result = parseInt(num1) - parseInt(num2)
+        this.setState({ result })
+    }
+
+    handleMultiply = () => {
+        const { num1, num2 } = this.state
+        const result = parseInt(num1) * parseInt(num2)
+        this.setState({ result })
+    }
+
+    handleDivide = () => {
+        const { num1, num2 } = this.state
+        const result = parseInt(num1) / parseInt(num2)
         this.setState({ result })
     }
 
@@ -33,7 +51,12 @@ export default class Calculator extends Component {
                         value={this.state.num1}
                         onChange={ (e) => this.setNum(e, 'num1') }
                     />
-                    <span>+</span>
+                    <span>
+                        <button onClick={this.handleSum}>+</button>
+                        <button onClick={this.handleSubtract}>-</button>
+                        <button onClick={this.handleMultiply}>*</button>
+                        <button onClick={this.handleDivide}>/</button>
+                    </span>
                     <input 
                         type="number" 
                         name="num2"
@@ -41,8 +64,8 @@ export default class Calculator extends Component {
                         value={this.state.num2}
                         onChange={ (e) => this.setNum(e, 'num2') }
                     />
-                    <button onClick={this.handleSubmit}>=</button>
-                    <h3>Sum: {this.state.result}</h3>
+                    <span>=</span>
+                    <h3>Result: {this.state.result}</h3>
                 </div>
             </div>
 
