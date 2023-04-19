c7ad335b33d2788e089d553137eb74ed4744a291
juanedcabrera <juanedcabrera@gmail.com>
2023-04-18 18:30:45 -0600

    Initial Solve with Bind

--- simple-calc/src/Calculator.js
+++ simple-calc/src/Calculator.js
@@ -11,13 +11,24 @@ class Calculator extends Component {
     };
 
     this.setNum = this.setNum.bind(this);
+    this.addNums = this.addNums.bind(this)
   }
 
   setNum(e) {
+    console.log(`Setting ${e.target.name} to ${e.target.value}`);
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  addNums() {
+    const num1 = Number(this.state.num1)
+    const num2 = Number(this.state.num2)
+
+    const sum = num1 + num2
+    this.setState( {result: sum} )
+  }
+
   render() {
+    console.log(`State: ${JSON.stringify(this.state)}`);
     return (
       <div className="container">
         <h1>Add with React!</h1>
@@ -25,7 +36,7 @@ class Calculator extends Component {
           <input type="text" name="num1" value={this.state.num1} onChange={this.setNum} />
           <span>+</span>
           <input type="text" name="num2" value={this.state.num2} onChange={this.setNum} />
-          <button>=</button>
+          <button onClick={this.addNums}>=</button>
           <h3>{this.state.result}</h3>
         </div>
       </div>
